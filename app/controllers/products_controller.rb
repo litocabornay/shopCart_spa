9dd0f1bdb4dd01a3586258440aef078329c29afb
Lito Cabornay <litocabornay91@gmail.com>
2018-09-14 19:53:52 +0800

    update main page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,6 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @order_item = current_order.order_items.new
+    @order_items = current_order.order_items
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.delete_all
-Product.create! id: 1, name: "Banana", price: 0.49, active: true
-Product.create! id: 2, name: "Apple", price: 0.29, active: true
-Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
+Product.create! id: 1, name: "Scotland Flag", price: 20, active: true
+Product.create! id: 2, name: "Children’s Car Seat", price: 33.90, active: true
+Product.create! id: 3, name: "Magnetic Wrist Band", price: 9.00, active: true
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
